a0edb45dd40ab7f05f617a872ebd07d3ead7dd1c
Nicolas Caceres Sala <ncsala@gmail.com>
2022-10-03 19:11:40 -0300

    Creando test para ruta POST /api/v1/pictures

--- Mi-ecommerce/api/test/pictures.test.js
+++ Mi-ecommerce/api/test/pictures.test.js
@@ -23,12 +23,10 @@ afterEach(() => {
 });
 
 beforeAll(async () => {
-  await db.sequelize.sync({force: true})
-
-	//Se borran las tablas de la base de datos
-	// destroyTables();
+	// Se la base de datos de testing
+	await db.sequelize.sync({ force: true });
 	//Se cargan con info para pruebas
-	loadingDataInTestingDB();
+	await loadingDataInTestingDB();
 });
 
 //CREAR UNA CAGTEGORIA
@@ -47,7 +45,7 @@ beforeAll(async () => {
 // });
 
 //Tests para crear una picture ---------------------------------------------------------------------
-describe('POST /pictures', () => {
+describe('POST /api/v1/pictures', () => {
 	it('should create a new picture in the database with status response 201', async () => {
 		//debo logueaerme desde la ruta o desde el test generando un token?
 		const token = await generateToken('god');
@@ -66,7 +64,7 @@ describe('POST /pictures', () => {
 			.expect('Content-Type', /json/)
 			.expect(201);
 
-		//testear que efectivamente este en la base de datos
+		// Se espera que la picture este creada en la base de datos
 		const picture = await db.Picture.findOne({
 			where: { picture_id: response.body.data.picture_id },
 		});
@@ -79,4 +77,70 @@ describe('POST /pictures', () => {
 			})
 		);
 	});
+
+  it('should return 404 if product does not exist', async () => {
+		const token = await generateToken('god');
+
+		const response = await request(app)
+			.get('/api/v1/pictures?product=4000')
+			.auth(token, { type: 'bearer' })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(404);
+		expect(response.body.msg).toBe('Product not found');
+	});
+
+	it('should return 400 if pictureUrl is not provided', async () => {
+		const token = await generateToken('god');
+
+		const response = await request(app)
+			.post('/api/v1/pictures')
+			.auth(token, { type: 'bearer' })
+			.send({
+				pictureDescription: 'Picture description',
+				productId: 1,
+			})
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(400);
+		expect(response.body.msg).toEqual('URL is required');
+	});
+
+	it('should return 400 if productId is not provided', async () => {
+		const token = await generateToken('god');
+
+		const response = await request(app)
+			.post('/api/v1/pictures')
+			.auth(token, { type: 'bearer' })
+			.send({
+				pictureUrl: 'http://www.una-linda-picture.com',
+				pictureDescription: 'Picture description',
+			})
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(400);
+		expect(response.body.msg).toBe('Product Id is required');
+	});
+
+	it('should return 500 if there is an error on the server', async () => {
+		const token = await generateToken('god');
+
+    //Simulo un error en la base de datos
+		const stub = sinon.stub(db.Picture, 'findAll').throws();
+
+		const response = await request(app)
+			.get('/api/v1/pictures?product=1')
+			.auth(token, { type: 'bearer' })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(500);
+		expect(response.body).toHaveProperty('error');
+		expect(response.body).toHaveProperty('msg');
+    expect(response.body.error).toBe(true);
+
+    //Restauro el metodo findAll
+		stub.restore();
+	});
 });
+//------------------------------------------------------------------------------
+
