766b88fd6c82d7c9706aa5ba826589917aeeee34
Nicolas Caceres Sala <ncsala@gmail.com>
2022-10-03 18:33:50 -0300

    sequelize.sync

--- Mi-ecommerce/api/test/helpers.js
+++ Mi-ecommerce/api/test/helpers.js
@@ -1,19 +1,21 @@
 const { generateJWT } = require('../../helpers/generateJWT');
-
 const users = require('../data.json/user.json');
 const products = require('../data.json/products.json');
 const categories = require('../data.json/categories.json');
 const pictures = require('../data.json/pictures.json');
 const cartProducts = require('../data.json/cartProduct.json');
 const db = require('../database/models');
+const { Op } = require('sequelize');
 
+// Genera JWT para pruebas con el rol pasado como argumento
 const generateToken = async (role) => {
 	return await generateJWT({
 		role: role,
 	});
 };
 
-async function carga(){
+// Carga datos en la base de datos
+async function loadingDataInTestingDB() {
 	for await (let u of users) {
 		await db.User.upsert({
 			first_name: u.first_name,
@@ -50,7 +52,7 @@ async function carga(){
 			title: product.title,
 			stock: product.stock,
 			mostwanted: product.mostwanted,
-  description: product.description,
+			description: product.description,
 			category_id: product.category,
 		});
 	}
@@ -73,4 +75,39 @@ async function carga(){
 	}
 }
 
-module.exports = { generateToken , carga};
+// Vacía las tablas para empezar de cero
+const destroyTables = async () => {
+	db.User.destroy({
+		where: {
+			user_id: {
+				[Op.gt]: 0,
+			},
+		},
+	});
+
+	db.Product.destroy({
+		where: {
+			product_id: {
+				[Op.gt]: 0,
+			},
+		},
+	});
+
+	db.Picture.destroy({
+		where: {
+			picture_id: {
+				[Op.gt]: 0,
+			},
+		},
+	});
+
+	db.Cart.destroy({
+		where: {
+			cart_id: {
+				[Op.gt]: 0,
+			},
+		},
+	});
+};
+
+module.exports = { generateToken, loadingDataInTestingDB, destroyTables };
--- Mi-ecommerce/api/test/pictures.test.js
+++ Mi-ecommerce/api/test/pictures.test.js
@@ -1,63 +1,35 @@
+// jest.useFakeTimers();
 const request = require('supertest');
 const { app, server } = require('../../server');
 const db = require('../database/models');
 const sinon = require('sinon');
-const {carga , generateToken } = require('./helpers');
-const { Op } = require('sequelize');
-
+const {
+	generateToken,
+	loadingDataInTestingDB,
+	destroyTables,
+} = require('./helpers');
 
 afterAll(() => {
-	// models.sequelize.close()
-	// await models.sequelize.sync({ force: true });
+	// db.sequelize.close()
+	// await db.sequelize.sync({ force: true });
 });
 
 afterEach(() => {
 	//borrar base de datos
-	// models.sequelize.sync({ force: true });
+	// db.sequelize.sync({ force: true });
 	// otra forma
-	// models.sequelize.drop();
+	// db.sequelize.drop();
 	server.close();
 });
 
-beforeAll(async ()=>{
+beforeAll(async () => {
+  await db.sequelize.sync({force: true})
 
-	db.User.destroy({
-		where:{
-			user_id:{
-				[Op.gt]:0
-			}
-		}
-	})
-
-	db.Product.destroy({
-		where:{
-			product_id:{
-				[Op.gt]:0
-			}
-		}
-	})
-
-	db.Picture.destroy({
-		where:{
-			picture_id:{
-				[Op.gt]:0
-			}
-		}
-	})
-
-	db.Cart.destroy({
-		where:{
-			cart_id:{
-				[Op.gt]:0
-			}
-		}
-	})
-	carga()
-})
-
-test('/',()=>{
-	expect(1).toBe(1);
-})
+	//Se borran las tablas de la base de datos
+	// destroyTables();
+	//Se cargan con info para pruebas
+	loadingDataInTestingDB();
+});
 
 //CREAR UNA CAGTEGORIA
 // describe('POST /api/v1/category', () => {
@@ -75,37 +47,36 @@ test('/',()=>{
 // });
 
 //Tests para crear una picture ---------------------------------------------------------------------
-// describe('POST /pictures', () => {
-// 	it('should create a new picture in the database with status response 201', async () => {
-// 		//debo logueaerme desde la ruta o desde el test generando un token?
-// 		const token = await generateToken('god');
+describe('POST /pictures', () => {
+	it('should create a new picture in the database with status response 201', async () => {
+		//debo logueaerme desde la ruta o desde el test generando un token?
+		const token = await generateToken('god');
 
-// 		const newPicture = {
-// 			pictureUrl: 'http://www.una-linda-picture.com',
-// 			pictureDescription: 'Picture description',
-// 			productId: 1,
-// 		};
+		const newPicture = {
+			pictureUrl: 'http://www.una-linda-picture.com',
+			pictureDescription: 'Picture description',
+			productId: 1,
+		};
 
-// 		const response = await request(app)
-// 			.post('/api/v1/pictures')
-// 			.auth(token, { type: 'bearer' })
-// 			.send(newPicture)
-// 			.set('Accept', 'application/json')
-// 			.expect('Content-Type', /json/)
-// 			.expect(201);
+		const response = await request(app)
+			.post('/api/v1/pictures')
+			.auth(token, { type: 'bearer' })
+			.send(newPicture)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(201);
 
-// 		//testear que efectivamente este en la base de datos
-// 		const picture = await db.Picture.findOne({
-// 			where: { picture_id: response.body.data.picture_id },
-// 		});
-// 		expect(picture).toEqual(
-// 			expect.objectContaining({
-// 				picture_id: response.body.data.picture_id,
-// 				picture_url: 'http://www.una-linda-picture.com',
-// 				picture_description: 'Picture description',
-// 				product_id: 1,
-// 			})
-// 		);
-// 	});
-
-// }) 
+		//testear que efectivamente este en la base de datos
+		const picture = await db.Picture.findOne({
+			where: { picture_id: response.body.data.picture_id },
+		});
+		expect(picture).toEqual(
+			expect.objectContaining({
+				picture_id: response.body.data.picture_id,
+				picture_url: 'http://www.una-linda-picture.com',
+				picture_description: 'Picture description',
+				product_id: 1,
+			})
+		);
+	});
+});
