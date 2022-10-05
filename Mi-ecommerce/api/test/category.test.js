c24b685ee942f783ee1521139bf295b4bff69a6f
Mokubag <juan.galvan@fing.edu.uy>
2022-10-05 16:24:41 -0300

    dos test 200 y 201

--- Mi-ecommerce/api/test/category.test.js
+++ Mi-ecommerce/api/test/category.test.js
@@ -5,15 +5,31 @@ const sinon = require('sinon');
 
 const {
 	generateToken,
-//	loadingDataInTestingDB,
+	cargarDatos,
 } = require('./helpers');
 
-//const { Op } = require("sequelize");
+const { Op } = require("sequelize");
+
+
+beforeAll(async () => {
+
+    await cargarDatos()
+});
 
 describe('/category GET',()=>{
-    test("debe devolver status 200", async ()=>{
+    test("must return a status 200 and with the correct json format", async ()=>{
 		const token = await generateToken('god');
 		const res = await request(app).get('/api/v1/category').auth(token,{type:'bearer'});
         expect(res.statusCode).toBe(200);
+	});
+
+	const newCategory ={
+		name:"hielo1424"
+	}
+
+	test("devolver status 201 al crear una categoria", async ()=>{
+		const token = await generateToken('god');
+		const res = await request(app).post('/api/v1/category').auth(token,{type:'bearer'}).send(newCategory);
+        expect(res.statusCode).toBe(201);
 	});
 });
