2eeb731eb3742b2656f09e6da57524b5db75ffb4
mBonzini <marcobonzini@gmail.com>
2022-10-06 00:33:04 -0300

    cart test al 97%, falta testear con guest

--- Mi-ecommerce/api/test/carts.test.js
+++ Mi-ecommerce/api/test/carts.test.js
@@ -1,7 +1,9 @@
+const { response } = require('express');
 const request = require('supertest');
 const {app} = require('../../server');
 const db = require('../database/models');
 const { cargarDatos, generateToken, generateTokenWithId } = require('./helpers');
+const sinon = require('sinon');
 
 
 
@@ -82,12 +84,141 @@ describe('Get /cart/:id', () => {
 
     })
 
-    test('Must return the cart of user number one using role "god" status 404', async () =>{
+    test('Must return error status 404 cart does not exist', async () =>{
         const token = await generateToken('god');
         const res =  await request(app).get('/api/v1/carts/10').auth(token, {type: 'bearer'});
         expect(res.statusCode).toBe(404);
+    })
 
+    test('Must return error status 404 cart does not exist', async () =>{
+        const token = await generateToken('god');
+        const res =  await request(app).get('/api/v1/carts/10').auth(token, {type: 'bearer'});
+        expect(res.statusCode).toBe(404);
+    })
+    test.skip('Must return error status 403 forbidden acces to another user cart', async () =>{
+        const token = await generateToken('guest');
+        const res =  await request(app).get('/api/v1/carts/2').auth(token, {type: 'bearer'});
+        expect(res.statusCode).toBe(403);
+    })
+    test('Must return error Ístatus 500 when there is a server error', async () =>{
+        const token = await generateToken('god');
+        const stub = sinon.stub(db.cart_product, 'findAll').throws();
+        const res =  await request(app).get('/api/v1/carts/1').auth(token, {type: 'bearer'});
+        expect(res.statusCode).toBe(500);
+        stub.restore();
+    })
+})
+
+describe('PUT /carts/id', () => {
+
+    test("Must insert a new product into a user's cart and return status 200 & the user's updated cart ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 4,
+            quantity: 1
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(200);
+        res.body.data.forEach(element => {
+          expect(element).toEqual(expect.objectContaining({
+            product_id: expect.any(Number),
+            quantity: expect.any(Number),
+            createdAt: expect.any(String),
+            createdAt: expect.any(String)
+        }))
+        expect(element.product_id).toBeGreaterThan(0);
+        expect(element.quantity).toBeGreaterThanOrEqual(0);
+        })
+    })
+
+    test("Must insert an existing product into a user's cart and return status 200 & the user's updated cart ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 2,
+            quantity: 1
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(200);
+        res.body.data.forEach(element => {
+          expect(element).toEqual(expect.objectContaining({
+            product_id: expect.any(Number),
+            quantity: expect.any(Number),
+            createdAt: expect.any(String),
+            createdAt: expect.any(String)
+        }))
+        expect(element.product_id).toBeGreaterThan(0);
+        expect(element.quantity).toBeGreaterThanOrEqual(0);
+        })
+    })
+
+    test("Must return status 400 when the new quantity is less than 0 ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 2,
+            quantity: -100
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(400);
 
     })
 
+    test("Must return error 404 when cart does not exist ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 4,
+            quantity: 1
+        }
+        const res = await request(app).put('/api/v1/carts/10').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(404);
+
+    })
+
+    test("Must return error 404 when product does not exist ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 400,
+            quantity: 1
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(404);
+
+    })
+
+    test("Must return error 404 when there is not enough stock ", async() => {
+        const token = await generateToken('god');
+        const newData = {
+            id: 4,
+            quantity: 1000
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'})
+                    .send(newData)
+                    .expect(404);
+    })
+
+    test("Must return error 403 when trying to edit another user's cart ", async() => {
+        const token = await generateToken('admin');
+        const newData = {
+            id: 4,
+            quantity: 1
+        }
+        const res = await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'}).send(newData);
+        expect(res.statusCode).toBe(403);
+
+    })
+    test('Must return error status 500 when there is a server error', async () =>{
+        const token = await generateToken('god');      
+        const newData = {
+            id: 4,
+            quantity: 1
+        }
+        const stub = sinon.stub(db.cart_product, 'findAll').throws();
+        const res =  await request(app).put('/api/v1/carts/1').auth(token, {type: 'bearer'}).send(newData);
+        expect(res.statusCode).toBe(500);
+        stub.restore();
+    })
 })
