96b4bd1261eff73ea1cb8bd8975600ea6d12999d
juanch013 <felipeparada99@hotmail.com>
2022-10-06 17:04:48 -0300

    modifico readme y traduzo al ingles algunos mensajes de los test

--- Mi-ecommerce/api/test/products.test.js
+++ Mi-ecommerce/api/test/products.test.js
@@ -53,8 +53,8 @@ describe('/products GET',()=>{
             expect(parseFloat(element.price)).toBeGreaterThan(0);
         });
     })
-
-    test('debe devolver status 401 y un json con error:true si se da token erroneo',async ()=>{
+    test( 'must return a statusCode 401 , in the json respone , must be an error attribute with the value true, and a data attribute with the correct format'
+    ,async ()=>{
         const token = await generateToken('g');
         let res = await request(app).get('/api/v1/products').auth(token,{type:'bearer'});
         expect(res.statusCode).toBe(401);
