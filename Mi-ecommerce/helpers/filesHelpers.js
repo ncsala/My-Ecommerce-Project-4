2fc871227d1f92c5ce3860308d7690fd19b68d45
juanch013 <felipeparada99@hotmail.com>
2022-10-06 11:39:11 -0300

    borro clg's innecesarios

--- Mi-ecommerce/helpers/filesHelpers.js
+++ Mi-ecommerce/helpers/filesHelpers.js
@@ -3,7 +3,6 @@ const db = require('../api/database/models');
 //se le pasa un id de categoria y si existe devuelve true sino false
 const existeCat = async (id) => {
 	let cat = await db.Category.findByPk(id);
-	console.log(cat);
 	if (cat != null) {
 		return true;
 	}
