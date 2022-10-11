e0867842cb37efa026a86f421c56bfcdf1f0d5a4
cabrerajuanjo <juanjosecabrera@G500606MAC346.local>
2022-10-11 12:08:19 -0300

    Mejora de coverage

--- Mi-ecommerce/api/database/config/config.js
+++ Mi-ecommerce/api/database/config/config.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 const config = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
+    username: process.env.DB_USER, //|| 'root',
+    password: process.env.DB_PASS, //|| null,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST, //|| '127.0.0.1',
     dialect: 'mysql',
     logging: false,
   }, 
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
+    username: process.env.DB_USER, //|| 'root',
+    password: process.env.DB_PASS, //|| null,
     database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST, //|| '127.0.0.1',
     dialect: 'mysql',
     logging: false,
   },
